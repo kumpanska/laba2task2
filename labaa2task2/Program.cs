ab04e5aa6a6f724715974b775dcca2065ea5b529
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-10-20 00:14:17 +0300

    add properties

--- labaa2task2/Program.cs
+++ labaa2task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,40 @@ namespace labaa2task2
         public override string ToString()
         {
             return $"Time:{hour}:{minute:D2}:{second:D2}";
+        }
+        public int Hour
+        {
+            get { return hour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException("Hour should be between 0 and 23");
+                }
+                hour = value;
+            }
+        }
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException("Minute should be between 0 and 59");
+                }
+                minute = value;
+            }
+        }
+        public int Second
+        {
+            get { return second; }
+            set { if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException("Second should be between 0 and 59");
+                }
+                second = value;
+            }
         }
     }
     internal class Program
