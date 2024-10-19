d2d967d4a82a5867c438d365ea2eda60f8d45bdd
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-10-19 23:31:59 +0300

    add public class MyTime and private fields

---
+++ labaa2task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaa2task2
+{
+    public class MyTime
+    {
+        private int hour;
+        private int minute;
+        private int second;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
